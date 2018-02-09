597c9e7d45aae7d31e805d4ebf19c3f54d81d26d
StanislavNi <stanislavni1991@gmail.com>
2018-02-09 18:34:50 +0500

    base/index.html

--- meetstar_site/mainpage/urls.py
+++ meetstar_site/mainpage/urls.py
@@ -4,11 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'', views.index, name='index'),
-    url(r'', views.carousel, name='carousel'),
-    url(r'', views.how_to, name='how_to'),
-    url(r'', views.events, name='events'),
-    url(r'', views.media, name='media'),
-    url(r'', views.contact, name='contact'),
-    url(r'', views.footer, name='footer'),
-    url(r'', views.login, name='login'),
 ]
--- meetstar_site/mainpage/views.py
+++ meetstar_site/mainpage/views.py
@@ -3,25 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'mainpage/base.html')
-
-def carousel(request):
-    return render(request, 'mainpage/carousel_partners.html')
-
-def how_to(request):
-    return render(request, 'mainpage/how_to.html')
-
-def events(request):
-    return render(request, 'mainpage/events.html')
-
-def media(request):
-     return render(request, 'mainpage/media.html')
-
-def contact(request):
-    return render(request, 'mainpage/contact.html')
-
-def footer(request):
-    return render(request, 'mainpage/footer.html')
-
-def login(request):
-    return render(request, 'mainpage/login.html')
+    return render(request, 'mainpage/index.html')

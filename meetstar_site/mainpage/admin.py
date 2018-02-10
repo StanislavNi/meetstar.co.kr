552835fd75539a7fcc6d4d692b85a9f9df369e3f
StanislavNi <stanislavni1991@gmail.com>
2018-02-11 00:35:05 +0500

    try to-upcoming events

--- meetstar_site/mainpage/admin.py
+++ meetstar_site/mainpage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Events
+from .models import UsersInEvent
 
-# Register your models here.
+admin.site.register(Events)
+admin.site.register(UsersInEvent)

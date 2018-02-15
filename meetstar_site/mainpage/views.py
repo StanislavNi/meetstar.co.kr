d723ba19e15b8d1c1be0644735d510d84fdc4df1
StanislavNi <stanislavni1991@gmail.com>
2018-02-15 20:01:51 +0500

    randomize call

--- meetstar_site/mainpage/views.py
+++ meetstar_site/mainpage/views.py
@@ -7,22 +7,19 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
-
 def index(request):
-    upcoming_events = Events.objects.filter(date__gt=datetime.datetime.now())
+    upcoming_events = Events.objects.filter(winner__isnull=True,
+                                            date__gt=datetime.datetime.now())
     ctx = {'upcoming_events': upcoming_events}
     return render(request, 'mainpage/index.html', context=ctx)
+
 
 def randomize(request):
     event_id = request.GET['event_id']
     try:
         event = Events.objects.get(id=event_id)
+        event.randomize()
     except Events.DoesNotExist:
         return HttpResponse('Event with ID {0} doesnt exist'.format(event_id))
 
     return HttpResponse(event)
-
-def upcoming(request):
-    up_events = request.GET['upcoming_events']
-    return HttpResponse(up_events)

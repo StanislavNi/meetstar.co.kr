61108a5076394cecd460438aedc9b2b1f2045f9d
StanislavNi <32291799+StanislavNi@users.noreply.github.com>
2018-02-17 06:58:01 +0500

    view and template (#6)

--- meetstar_site/mainpage/urls.py
+++ meetstar_site/mainpage/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^randomize$', views.randomize, name=''),
+    url(r'^upcoming$', views.upcoming, name=''),
+    url(r'^account_page$', views.account_page, name=''),
 ]
--- meetstar_site/mainpage/views.py
+++ meetstar_site/mainpage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Events
+from django.contrib.auth.models import User
 import datetime
 
 import logging
@@ -10,7 +11,7 @@ logger = logging.getLogger(__name__)
 def index(request):
     upcoming_events = Events.objects.filter(winner__isnull=True,
                                             date__gt=datetime.datetime.now())
-    ctx = {'upcoming_events': upcoming_events}
+    ctx = {'upcoming_events': upcoming_events, 'is_auth': request.user.is_authenticated}
     return render(request, 'mainpage/index.html', context=ctx)
 
 def randomize(request):
@@ -22,3 +23,12 @@ def randomize(request):
         return HttpResponse('Event with ID {0} doesnt exist'.format(event_id))
 
     return HttpResponse(event)
+
+
+def upcoming(request):
+    up_events = request.GET.get['event']
+    return HttpResponse(up_events)
+
+def account_page(request):
+    ctx = {'user': request.user}
+    return render(request, 'mainpage/account_page.html', context=ctx)

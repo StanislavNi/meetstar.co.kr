059e8c08609b49e6ce5e12ace20ad2aa6f7ce034
StanislavNi <stanislavni1991@gmail.com>
2018-02-17 22:34:25 +0500

    try to make loginpage with authentication

--- meetstar_site/mainpage/urls.py
+++ meetstar_site/mainpage/urls.py
@@ -1,10 +1,16 @@
+from django.conf import settings
 from django.conf.urls import url
-from . import views
+from django.conf.urls.static import static
 
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^randomize$', views.randomize, name=''),
-    url(r'^upcoming$', views.upcoming, name=''),
     url(r'^account_page$', views.account_page, name=''),
+    url(r'^ login /$', views.login, name ='login'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

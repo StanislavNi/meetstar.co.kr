39a20997d2752915d9c35934d19a5171d18bca0f
StanislavNi <stanislavni1991@gmail.com>
2018-03-04 19:12:46 +0500

    bootstrap added

--- meetstar_site/meetstar_site/settings.py
+++ meetstar_site/meetstar_site/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainpage',
     'content',
-    'userprofile'
+    'userprofile',
+    'bootstrapform',
 ]
 
 AUTH_PROFILE_MODULE = AUTHENTICATION_BACKENDS = (
